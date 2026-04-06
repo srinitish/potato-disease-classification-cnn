2eca3449e2a4e05e8c5e0e76ed12cbec06226f5e
srinitish <ss.srinitish@gmail.com>
2026-04-06 18:38:56 +0530

    first sava

---
+++ api/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("../models/1.keras")
+CLASS_NAMES = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+
+@app.get("/ping")
+async def ping():
+    return "hi iam alive"
+
+def read_file_as_image(data)->np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+): 
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image,0)
+    prediction = MODEL.predict(img_batch)
+    index = np.argmax(prediction[0])
+    predicted_class = CLASS_NAMES[index]
+    confidence = np.max(prediction[0])
+    return{
+        'class':predicted_class,
+        'confidence':float(confidence)
+    }
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='localhost',port =8000)
